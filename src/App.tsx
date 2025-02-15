98151ad62be67e1316b9775493bbd5534c404228
Sathwik Dasari <adityasathwikdv1772003@gmail.com>
2025-02-15 19:49:45 +0530

    done changes in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 
 import './App.css'
 import Home from './Page'
 
 function App() {
-  
+
 
   return (
     <>
-<Home/>
+      <Home />
 
     </>
   )
